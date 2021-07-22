2e0cd57a611b2ff21160f65cd4f3c07d135dd899
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 13:55:16 +0300

    Created class ForChildren and enum of subtypes

---
+++ PharmacyOOP/Abstractions/ForChildren.cs
@@ -0,0 +1,12 @@
+﻿using PharmacyOOP.Enums;
+
+namespace PharmacyOOP.Abstractions
+{
+    public abstract class ForChildren:Product
+    {
+        public ForChildren()
+        {
+            Type = ProductTypes.ForChildren;
+        }
+    }
+}
