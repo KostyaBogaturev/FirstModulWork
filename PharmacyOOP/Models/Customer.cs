d7f1a402a3c279a82df7fc2a3e0d62041bd3a32b
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 14:22:32 +0300

    Created class Customer

---
+++ PharmacyOOP/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using PharmacyOOP.Abstractions;
+using PharmacyOOP.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace PharmacyOOP.Models
+{
+    public class Customer : User
+    {
+        public Customer()
+        {
+            Role = Roles.Customer;
+        }
+
+        public override void AddToOrder(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MakeOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
