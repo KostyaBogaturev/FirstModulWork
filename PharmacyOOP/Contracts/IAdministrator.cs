f7fc2a1ead768b69ff1d1428ad12f9b4c7483f8c
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 13:37:13 +0300

    Created new interface

---
+++ PharmacyOOP/Contracts/IAdministrator.cs
@@ -0,0 +1,14 @@
+﻿using PharmacyOOP.Abstractions;
+using System;
+
+namespace PharmacyOOP.Contracts
+{
+    public interface IAdministrator
+    {
+        void Create(Product product);
+
+        void Remove(Guid Id);
+
+        void Update(Guid id, Product product);
+    }
+}
