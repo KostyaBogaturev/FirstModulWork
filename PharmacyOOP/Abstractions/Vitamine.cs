3e495b50ac549d7c43744ae8d7f29200df081cc1
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 13:50:05 +0300

    Created adlstact class Vitamine and subtype enum for this

---
+++ PharmacyOOP/Abstractions/Vitamine.cs
@@ -0,0 +1,15 @@
+﻿using PharmacyOOP.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyOOP.Abstractions
+{
+    public abstract class Vitamine: Product
+    {
+        public Vitamine()
+        {
+            Type= ProductTypes.Vitamine
+        }
+    }
+}
