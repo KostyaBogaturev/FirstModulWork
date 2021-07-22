1c3696a0099bd505821fc2cef0945b4636bb7e17
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 13:19:10 +0300

    Added class order

---
+++ PharmacyOOP/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using PharmacyOOP.Abstractions;
+using System;
+
+namespace PharmacyOOP.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        public IEquatable<Product> Products { get; set; }
+
+        public Guid UserId { get; set; }
+
+    }
+}
