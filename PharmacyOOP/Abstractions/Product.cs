8e9c2fc76e0e7b40f3a41d82b0f0cb22f8d37b11
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 13:04:02 +0300

    Created abstract Product class

---
+++ PharmacyOOP/Abstractions/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PharmacyOOP.Abstractions
+{
+    public abstract class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Firm { get; set; }
+
+        public double Cost { get; set; }
+
+        public int Count { get; set; }
+
+        public string Image { get; set; }
+    }
+}
