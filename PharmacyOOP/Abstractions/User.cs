b9efcc626dbbca04d6463e2b7dda0f9ebd3154fe
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 13:01:55 +0300

    Created User abstract class

---
+++ PharmacyOOP/Abstractions/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyOOP.Abstractions
+{
+    public abstract class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Login { get; set; }
+
+        public string HashPassword { get; set; }
+
+        public string Email { get; set; }
+
+
+    }
+}
