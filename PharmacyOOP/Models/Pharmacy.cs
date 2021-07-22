da966d4f135df88fe28908f9be8325c82d01ec02
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 13:15:07 +0300

    Added class Pharmacy

---
+++ PharmacyOOP/Models/Pharmacy.cs
@@ -0,0 +1,18 @@
+﻿using PharmacyOOP.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyOOP.Models
+{
+    public class Pharmacy
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
