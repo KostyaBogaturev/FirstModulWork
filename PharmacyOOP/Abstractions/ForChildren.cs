195e3a7b9031c7f57a7ccf02901434403a6aa83b
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 13:55:54 +0300

    New property for class ForChildren

--- PharmacyOOP/Abstractions/ForChildren.cs
+++ PharmacyOOP/Abstractions/ForChildren.cs
@@ -4,6 +4,8 @@ namespace PharmacyOOP.Abstractions
 {
     public abstract class ForChildren:Product
     {
+        public SubtypesForChildren Subtype { get; set; }
+
         public ForChildren()
         {
             Type = ProductTypes.ForChildren;
