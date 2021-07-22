e6ff5367f54354cf6a2b9af165b1f965fca9bfa3
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-22 14:24:21 +0300

    Created class Administrator

---
+++ PharmacyOOP/Models/Administrator.cs
@@ -0,0 +1,46 @@
+﻿using PharmacyOOP.Abstractions;
+using PharmacyOOP.Contracts;
+using PharmacyOOP.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace PharmacyOOP.Models
+{
+    public class Administrator : User, IAdministrator
+    {
+        public Administrator()
+        {
+            Role = Roles.Administrator;
+        }
+
+        public override void AddToOrder(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MakeOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
