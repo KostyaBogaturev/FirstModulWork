7cf4389ac9f563d44900b9308ee6c4450ccac684
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-23 15:00:01 +0300

    updated ChildHygien

--- PharmacyOOP/Models/ChildHygien.cs
+++ PharmacyOOP/Models/ChildHygien.cs
@@ -5,6 +5,8 @@ namespace PharmacyOOP.Models
 {
     public class ChildHygien:ForChildren
     {
+        public int AgeRestriction { get; set; }
+
         public ChildHygien()
         {
             Subtype = SubtypesForChildren.ChildHygien;
