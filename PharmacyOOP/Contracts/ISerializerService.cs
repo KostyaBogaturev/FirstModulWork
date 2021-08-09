6d481c08ef665c6507fef8dd6822ffdc9493a55e
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-08-09 19:22:59 +0300

    Json Serializer fich added

---
+++ PharmacyOOP/Contracts/ISerializerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyOOP.Contracts
+{
+    public interface ISerializerService<T>
+    {
+        void Serialize(T item);
+
+        IEnumerable<T> Deserialize();
+    }
+}
---
+++ PharmacyOOP/Serviceses/SerializerService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using PharmacyOOP.Contracts;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PharmacyOOP.Serviceses
+{
+    public class SerializerService<T> : ISerializerService<T>
+    {
+        private JsonSerializer _jsonSerializer = new JsonSerializer();
+
+        public string Path { get; set; }
+
+        public SerializerService(string path)
+        {
+            Path = path;
+        }
+
+        public IEnumerable<T> Deserialize()
+        {
+            IEnumerable<T> result;
+
+            using (StreamReader sr = new StreamReader(Path))
+            {
+                result = (IEnumerable<T>)_jsonSerializer.Deserialize(sr, typeof(IEnumerable<T>));
+            }
+            return result;
+        }
+
+        public void Serialize(T item)
+        {
+            using (StreamWriter sw = new StreamWriter(Path))
+            {
+                _jsonSerializer.Serialize(sw, item);
+            }
+        }
+    }
+}
