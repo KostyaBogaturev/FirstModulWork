a596d9040af5d6770d1874e425e3ffeda521bc23
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-08-09 14:26:35 +0300

    Added interface ISerializer<T>

---
+++ PharmacyOOP/Contracts/ISerializer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PharmacyOOP.Contracts
+{
+    public interface ISerializer<T>
+    {
+        void Serialize(T item);
+
+        IEnumerable<T> Deserialize();
+    }
+}
